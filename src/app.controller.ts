5a16e69df6d43fac0cfa4aaf3a380e535377571e
MamunCrafts <s1611177146@ru.ac.bd>
2025-01-01 15:55:21 +0600

    valkey redis

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+// app.controller.ts
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private appService: AppService) {}
+
+  @Post('setCache')
+  async setCache(@Body() body: { key: string; value: string }) {
+    const { key, value } = body;
+    console.log(body);
+    const response = await this.appService.setCache(key, value);
+    return { message: `Cache set with key: ${key}`, response };
+  }
+
+  @Get('getCache')
+  async getCache() {
+    const value = await this.appService.getCache();
+    return { value };
+  }
+
+  @Get('getCacheByKey')
+  async getCacheByKey(@Body() body: { key: string }) {
+    const { key } = body;
+    const value = await this.appService.getCacheByKey(key);
+    return { value };
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+import { createClient, RedisClientType } from 'redis';
+import { ConfigService } from '@nestjs/config';
+@Injectable()
+export class AppService {
+  private client: RedisClientType;
+
+  constructor(private configService: ConfigService) {
+    const redisUrl = this.configService.get<string>('REDIS_URL');
+    console.log('Connecting to Redis at:', redisUrl, process.env.REDIS_URL);
+    this.client = createClient({
+      url: redisUrl,
+    });
+
+    this.client.on('connect', () => {
+      console.log('Connected to Redis');
+    });
+
+    this.client.on('error', (err) => {
+      console.error('Redis error: ', err);
+    });
+  }
+
+  // Ensure the Redis client is connected before using it
+  async onModuleInit() {
+    await this.client.connect();
+  }
+
+  // Get value from Redis
+  async getCache(): Promise<string | null> {
+    try {
+      await this.client.set('test', 'Hello World from Valkey Redis');
+      return await this.client.get('test');
+    } catch (err) {
+      throw new Error(`Failed to get value from Redis: ${err}`);
+    }
+  }
+
+  async setCache(key: string, value: string): Promise<string> {
+    try {
+      return await this.client.set(key, value);
+    } catch (err) {
+      throw new Error(`Failed to set value in Redis: ${err}`);
+    }
+  }
+  async getCacheByKey(key: string): Promise<string | null> {
+    try {
+      return await this.client.get(key);
+    } catch (err) {
+      throw new Error(`Failed to get value from Redis: ${err}`);
+    }
+  }
+  // Optionally, you could add a method to close the Redis connection gracefully
+  async onModuleDestroy() {
+    await this.client.quit();
+  }
+}
